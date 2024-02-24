fe0bf96d2f69341be960a6665c0df94e638cf85d
jérémie THERY <thery.jeremie@yahoo.fr>
2024-02-24 23:20:12 +0100

    creation des cards

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,19 @@
 
 import '../styles/App.css';
 import Banner from './component/Banner';
+import LstMenu from './clients/lst_menus';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <div>
+        <LstMenu />
+      </div>
     </div>
   );
+
+  
 }
 
 export default App;
