1bb51864ecf731e2d74a3d47f1d84a880e2a624f
jérémie THERY <thery.jeremie@yahoo.fr>
2024-02-25 22:03:05 +0100

    css

--- src/pages/clients/menu_item.js
+++ src/pages/clients/menu_item.js
@@ -3,11 +3,14 @@ import '../../styles/menuItem.css'
 function menu_item({id, title, plats, description , dessert , boissons}){
 return(
  <li key ={id} class='menuItem'>
-    <p>
-    {title}</p>
+    <h2>{title}</h2>
+    <h3 class ='title'>Plats</h3>
     <p>{plats}</p>
+    <h3 class ='title'>description</h3>
     <p>{description}</p>
+    <h3 class ='title'>dessert</h3>
     <p>{dessert}</p>
+    <h3 class ='title'>boissons</h3>
     <p>{boissons}</p>
  </li>
 )
