87e6dc31975a9d6ea2070dfbb094e6b368b3ca77
jérémie THERY <thery.jeremie@yahoo.fr>
2024-02-25 21:29:43 +0100

    Remise ne place de app.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -10,25 +10,7 @@ function App() {
       <Banner />
       <div>
         <LstMenu />
-      </div>
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Banner />
-      <div>
-        <LstMenu />
-      </div>
+      </div>  
     </div>
   );
 
